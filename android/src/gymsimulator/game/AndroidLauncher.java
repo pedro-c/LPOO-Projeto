70298f403fed846b0559c2141ba82aeed5d6e149
pedro-c <pedrodc23@gmail.com>
2016-05-09 12:04:45 +0100

    working runner gif

---
+++ android/src/gymsimulator/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package gymsimulator.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import gymsimulator.game.gymSimulator;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Runner(), config);
+	}
+}
