f9281b623018076b1f6d20ffa26b13fea079d02c
pedro-c <pedrodc23@gmail.com>
2016-05-10 12:17:13 +0100

    hud created

--- core/src/gymsimulator/game/Screens/PlayScreen.java
+++ core/src/gymsimulator/game/Screens/PlayScreen.java
@@ -4,7 +4,14 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import gymsimulator.game.Scenes.Hud;
 import gymsimulator.game.gymSimulator;
 
 
@@ -13,11 +20,15 @@ import gymsimulator.game.gymSimulator;
  */
 public class PlayScreen implements Screen {
     private gymSimulator game;
-    Texture texture;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
+    private Hud hud;
 
     public PlayScreen(gymSimulator game){
         this.game=game;
-        texture = new Texture("badlogic.jpg");
+        gamecam = new OrthographicCamera();
+        gamePort = new FitViewport(gymSimulator.V_WIDTH, gymSimulator.V_HEIGHT,gamecam );
+        hud = new Hud(game.batch);
     }
 
     @Override
@@ -29,14 +40,14 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.begin();
-        game.batch.draw(texture, 0,0);
-        game.batch.end();
+
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        gamePort.update(width,height);
     }
 
     @Override
--- core/src/gymsimulator/game/gymSimulator.java
+++ core/src/gymsimulator/game/gymSimulator.java
@@ -8,6 +8,8 @@ import gymsimulator.game.Screens.PlayScreen;
 
 public class gymSimulator extends Game {
 
+	public static final int V_WIDTH = 400;
+	public static final int V_HEIGHT = 208;
 	public SpriteBatch batch;
 	
 	@Override
