f409258d2cbda579fd1378fcde2333aeb4a25a71
pedro-c <pedrodc23@gmail.com>
2016-05-31 17:44:41 +0100

    treadmill logic completed

--- core/src/gymsimulator/game/Logic/TreadmillLogic.java
+++ core/src/gymsimulator/game/Logic/TreadmillLogic.java
@@ -2,6 +2,8 @@ package gymsimulator.game.Logic;
 
 import com.badlogic.gdx.Gdx;
 
+import java.util.Random;
+
 /**
  * Created by pedro on 30/05/2016.
  */
@@ -11,25 +13,39 @@ public class TreadmillLogic {
     public int score;
     public int startTimer=0;
     public boolean endGame=false;
+    public float footPrint_x;
+    public float footPrint_y = Gdx.graphics.getHeight()/3;
     public TreadmillLogic(){
         rightScreen=0;
     }
-
+    int rand;
 
     public int update(float dt){
 
+        if(rightScreen==0){
+            Random random = new Random();
+            rand = random.nextInt(4) + 1;
+            if(rand==1 || rand==3){
+                footPrint_x = Gdx.graphics.getWidth()/2+ Gdx.graphics.getWidth()/6;
+            }else if(rand == 2 || rand==4){
+                footPrint_x = Gdx.graphics.getWidth()/3;
+            }
+            rightScreen=1;
+        }
+
         if(Gdx.input.isTouched()){
-            if(endGame){
+            if(endGame==false){
                 if(Gdx.input.getX() > Gdx.graphics.getWidth()/2){
-                    if(rightScreen==0){
+                    if(rand==1 || rand==3){
                         startTimer=1;
-                        rightScreen=1;
+                        rightScreen=0;
                         score++;
                         return 1;
                     }
                     else return -1;
                 }else {
-                    if (rightScreen == 1) {
+                    if (rand == 2 || rand==4) {
+                        startTimer=1;
                         rightScreen = 0;
                         score++;
                         return 1;
--- core/src/gymsimulator/game/States/RunnerGameState.java
+++ core/src/gymsimulator/game/States/RunnerGameState.java
@@ -27,7 +27,8 @@ public class RunnerGameState implements Screen {
 
     TreadmillLogic tmLogic;
     Animation walkAnimation;          // #3
-    Texture walkSheet;              // #4
+    Texture walkSheet;
+    Texture footPrint;
     TextureRegion[] walkFrames;             // #5
     SpriteBatch spriteBatch;            // #6
     TextureRegion currentFrame;           // #7
@@ -39,6 +40,7 @@ public class RunnerGameState implements Screen {
     public RunnerGameState(gymSimulator game) {
         hud = new Hud(game.batch);
         walkSheet = new Texture("flooranimation2.png"); // #9
+        footPrint = new Texture("footprint.png");
         TextureRegion[][] tmp = TextureRegion.split(walkSheet, walkSheet.getWidth()/FRAME_COLS, walkSheet.getHeight()/FRAME_ROWS);              // #10
         walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
         int index = 0;
@@ -50,16 +52,16 @@ public class RunnerGameState implements Screen {
         walkAnimation = new Animation(0.025f, walkFrames);      // #11
         spriteBatch = new SpriteBatch();                // #12
         stateTime = 0f;                         // #13
-
         tmLogic = new TreadmillLogic();
     }
 
 
     public void update(float dt){
         float flag=tmLogic.update(dt);
-        float acceleration=1;
-        float maxSpeed=5;
-        if(tmLogic.endGame==false){
+        float acceleration=5;
+        float maxSpeed=50;
+
+
             if(flag == -1){
                 if(treadSpeed - acceleration > 0){
                     treadSpeed-=acceleration;
@@ -68,15 +70,13 @@ public class RunnerGameState implements Screen {
                 }
             }else{
                 if(treadSpeed<maxSpeed)
-                    treadSpeed+=acceleration;
+                    treadSpeed+=acceleration*2;
                 else{
                     treadSpeed=maxSpeed;
                 }
             }
             hud.setLabelPlay(((Integer)tmLogic.score).toString());
-        }else{
-            treadSpeed=0;
-        }
+
 
     }
 
@@ -100,6 +100,7 @@ public class RunnerGameState implements Screen {
         currentFrame = walkAnimation.getKeyFrame(stateTime * treadSpeed, true);
         spriteBatch.begin();
         spriteBatch.draw(currentFrame, 200, 10,Gdx.graphics.getWidth()-400,Gdx.graphics.getHeight()-150);             // #17
+        spriteBatch.draw(footPrint, tmLogic.footPrint_x, tmLogic.footPrint_y, 100, 250 );
         spriteBatch.end();
         hud.stage.draw();
 
