fdbc3901ad8437e2afe266552f17719f9a0822ed
pedro-c <pedrodc23@gmail.com>
2016-06-04 02:32:38 +0100

    textures implemented

--- core/src/gymsimulator/game/Logic/MultiplayerLogic.java
+++ core/src/gymsimulator/game/Logic/MultiplayerLogic.java
@@ -11,6 +11,16 @@ public class MultiplayerLogic {
     public int redHealth= Gdx.graphics.getHeight()-2*Gdx.graphics.getHeight()/12;
     public boolean playerRedDefending=false;
     public boolean playerBlueDefending=false;
+    public boolean playerRedDAttacking=false;
+    public boolean playerBlueAttacking=false;
+    public float deltaBcounter=0;
+    public float deltaRcounter=0;
+    public boolean playerRedStartDefend=false;
+    public boolean playerBlueStartDefend=false;
+    public int buttonRSsize=0;
+    public int buttonBSsize=0;
+    private boolean endGame=false;
+
 
     public MultiplayerLogic(){
 
@@ -18,25 +28,58 @@ public class MultiplayerLogic {
 
     public int update(float dt) {
 
+
+        if(blueHealth==0 || redHealth==0)
+            endGame=true;
+        if(!endGame) {
+            float delta = Gdx.graphics.getDeltaTime();
+            if (playerRedStartDefend == true && deltaRcounter < 225) {
+                deltaRcounter++;
+                buttonRSsize++;
+                playerRedDefending = true;
+            } else if (playerRedStartDefend == false && deltaRcounter > 0) {
+                deltaRcounter--;
+                buttonRSsize--;
+                playerRedDefending = false;
+            } else
+                playerRedDefending = false;
+
+            if (playerBlueStartDefend == true && deltaBcounter < 225) {
+                deltaBcounter++;
+                buttonBSsize++;
+                playerBlueDefending = true;
+            } else if (playerBlueStartDefend == false && deltaBcounter > 0) {
+                deltaBcounter--;
+                buttonBSsize--;
+                playerBlueDefending = false;
+            } else
+                playerBlueDefending = false;
+        }
         return 0;
     }
 
     public void playerRedAttacking(){
         if(!playerBlueDefending){
-            blueHealth-=25;
+            if(blueHealth>0)
+                blueHealth-=25;
         }
+        playerRedDAttacking=true;
+
     }
     public void playerBlueAttacking(){
         if(!playerRedDefending){
-            redHealth-=25;
+            if(redHealth>0)
+                redHealth-=25;
         }
+        playerBlueAttacking=true;
     }
 
     public void setPlayerRedDefending(boolean bool){
-        playerRedDefending=bool;
+        playerRedStartDefend=bool;
+
     }
     public void setPlayerBlueDefending(boolean bool){
-        playerBlueDefending=bool;
+        playerBlueStartDefend=bool;
     }
 
 
--- core/src/gymsimulator/game/States/MultiplayerFight.java
+++ core/src/gymsimulator/game/States/MultiplayerFight.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import gymsimulator.game.Logic.MultiplayerLogic;
+import gymsimulator.game.Scenes.Hud;
 import gymsimulator.game.gymSimulator;
 
 /**
@@ -25,20 +26,45 @@ public class MultiplayerFight implements Screen {
     private Texture fistB;
     private Texture shieldR;
     private Texture shieldB;
+
+    //Red Player
+    private Texture RPlayer;
+    private Texture fistRRA;
+    private Texture fistRLA;
+    private Texture fistRRD;
+    private Texture fistRLD;
+    private Texture RRDD;
+    private Texture RLDD;
+
+    //Blue Player
+    private Texture BPlayer;
+    private Texture fistBRA;
+    private Texture fistBLA;
+    private Texture fistBRD;
+    private Texture fistBLD;
+    private Texture BRDD;
+    private Texture BLDD;
+
+    //Ring
+    private Texture ring;
+
     private Image buttonFistR;
     private Image buttonFistB;
     private Image buttonShieldR;
     private Image buttonShieldB;
     private Texture healthBarRed;
     private Texture healthBarGreen;
+    private Texture buttonShadow;
     SpriteBatch spriteBatch;
     public Stage stage;
+    private Hud hud;
 
 
 
     public MultiplayerFight(gymSimulator game){
 
         this.game=game;
+        hud = new Hud(game.batch);
         stage = new Stage();
         mpLogic = new MultiplayerLogic();
         fistR = new Texture("fistR.png");
@@ -47,6 +73,30 @@ public class MultiplayerFight implements Screen {
         shieldB = new Texture("shieldB.png");
         healthBarGreen = new Texture("HBgreen.png");
         healthBarRed = new Texture("HBred.png");
+        buttonShadow = new Texture("buttonShadow1.png");
+
+
+        //Red Player
+        RPlayer = new Texture("Rplayer.png");
+        fistRRA = new Texture("RRA.png");
+        fistRLA = new Texture("RLA.png");
+        fistRRD = new Texture("RRD.png");
+        fistRLD = new Texture("RLD.png");
+        RRDD = new Texture("RRDD.png");
+        RLDD = new Texture("RLDD.png");
+
+        //Blue Player
+        BPlayer = new Texture("Bplayer.png");
+        fistBRA = new Texture("BRA.png");
+        fistBLA = new Texture("BLA.png");
+        fistBRD = new Texture("BRD.png");
+        fistBLD = new Texture("BLD.png");
+        BRDD = new Texture("BRDD.png");
+        BLDD = new Texture("BLDD.png");
+
+        //Ring
+        ring = new Texture("ring.png");
+
         buttonFistB = new Image(fistB);
         buttonFistR = new Image(fistR);
         buttonShieldB = new Image(shieldB);
@@ -127,19 +177,49 @@ public class MultiplayerFight implements Screen {
 
         spriteBatch.begin();
 
-        spriteBatch.draw(fistB, 100, Gdx.graphics.getHeight()/12, 250, 250 );
-        spriteBatch.draw(fistR, Gdx.graphics.getWidth()-350, Gdx.graphics.getHeight()/12, 250, 250 );
-        spriteBatch.draw(shieldB, 100, Gdx.graphics.getHeight()-Gdx.graphics.getHeight()/12-250, 250, 250 );
-        spriteBatch.draw(shieldR, Gdx.graphics.getWidth()-350, Gdx.graphics.getHeight()-Gdx.graphics.getHeight()/12-250, 250, 250 );
+        spriteBatch.draw(fistB, 50, Gdx.graphics.getHeight()/12, 250, 250 );
+        spriteBatch.draw(fistR, Gdx.graphics.getWidth()-300,Gdx.graphics.getHeight()-Gdx.graphics.getHeight()/12-250 , 250, 250 );
+        spriteBatch.draw(shieldB, 50, Gdx.graphics.getHeight()-Gdx.graphics.getHeight()/12-250, 250, 250 );
+        spriteBatch.draw(shieldR, Gdx.graphics.getWidth()-300, Gdx.graphics.getHeight()/12, 250, 250 );
+
+        spriteBatch.draw(buttonShadow, 50+130-mpLogic.buttonBSsize/2, Gdx.graphics.getHeight()-Gdx.graphics.getHeight()/12-250+125-mpLogic.buttonBSsize/2, mpLogic.buttonBSsize, mpLogic.buttonBSsize );
+        spriteBatch.draw(buttonShadow, Gdx.graphics.getWidth()-300+120-mpLogic.buttonRSsize/2, Gdx.graphics.getHeight()/12-250+125-mpLogic.buttonRSsize/2, mpLogic.buttonRSsize, mpLogic.buttonRSsize );
 
 
-        spriteBatch.draw(healthBarRed, Gdx.graphics.getWidth()-450, 100+(Gdx.graphics.getHeight()-2*Gdx.graphics.getHeight()/12), 50,Gdx.graphics.getHeight()-2*Gdx.graphics.getHeight()/12);
-        spriteBatch.draw(healthBarGreen, Gdx.graphics.getWidth()-450,100+(Gdx.graphics.getHeight()-2*Gdx.graphics.getHeight()/12-mpLogic.redHealth), 50,  mpLogic.redHealth );
+        spriteBatch.draw(healthBarRed, Gdx.graphics.getWidth()-400, 100, 50,Gdx.graphics.getHeight()-2*Gdx.graphics.getHeight()/12);
+        spriteBatch.draw(healthBarGreen, Gdx.graphics.getWidth()-400, 100, 50,  mpLogic.redHealth );
 
-        spriteBatch.draw(healthBarRed, 400, 100+(Gdx.graphics.getHeight()-2*Gdx.graphics.getHeight()/12), 50,  Gdx.graphics.getHeight()-2*Gdx.graphics.getHeight()/12 );
-        spriteBatch.draw(healthBarGreen, 400, 100+(Gdx.graphics.getHeight()-2*Gdx.graphics.getHeight()/12-mpLogic.blueHealth), 50, mpLogic.blueHealth );
+        spriteBatch.draw(healthBarRed, 350, 100, 50,  Gdx.graphics.getHeight()-2*Gdx.graphics.getHeight()/12 );
+        spriteBatch.draw(healthBarGreen, 350, 100+(Gdx.graphics.getHeight()-2*Gdx.graphics.getHeight()/12-mpLogic.blueHealth), 50, mpLogic.blueHealth );
+
+        //draw Ring
+        spriteBatch.draw(ring, 425, 0, Gdx.graphics.getWidth()-850,Gdx.graphics.getHeight());
+
+
+
+        //draw Player Blue
+        spriteBatch.draw(BPlayer, 600, Gdx.graphics.getHeight()/2-125, 201, 252);
+        if(mpLogic.playerBlueDefending){
+            spriteBatch.draw(BLDD, 600, Gdx.graphics.getHeight()/2-125+252, 232, 88);
+            spriteBatch.draw(BRDD, 600, Gdx.graphics.getHeight()/2-125, 232, 88);
+        }
+
+        //draw Player Red
+        spriteBatch.draw(RPlayer, Gdx.graphics.getWidth()-800, Gdx.graphics.getHeight()/2-125, 201, 252);
+        if(mpLogic.playerBlueDefending){
+            spriteBatch.draw(RRDD, Gdx.graphics.getWidth()-800, Gdx.graphics.getHeight()/2-125+252, 232, 88);
+            spriteBatch.draw(RLDD, Gdx.graphics.getWidth()-800, Gdx.graphics.getHeight()/2-125, 232, 88);
+        }if(mpLogic.playerRedDAttacking){
+            spriteBatch.draw(fistRLA, Gdx.graphics.getWidth()-800, Gdx.graphics.getHeight()/2-125+252, 232, 88);
+            spriteBatch.draw(fistRRD, Gdx.graphics.getWidth()-800, Gdx.graphics.getHeight()/2-125, 232, 88);
+        }else{
+            spriteBatch.draw(fistRLD, Gdx.graphics.getWidth()-800, Gdx.graphics.getHeight()/2-125+252, 232, 88);
+            spriteBatch.draw(fistRRD, Gdx.graphics.getWidth()-800, Gdx.graphics.getHeight()/2-125, 232, 88);
+
+        }
 
         spriteBatch.end();
+        hud.stage.draw();
 
 
 
