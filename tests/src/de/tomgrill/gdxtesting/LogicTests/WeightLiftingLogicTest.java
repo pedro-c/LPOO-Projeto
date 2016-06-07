f59b9636da8d65b169ba8782c29e6bcb8696763c
Tiago Grosso <tiagogrosso99@gmail.com>
2016-06-07 18:17:41 +0100

    Refactored for private members

--- tests/src/de/tomgrill/gdxtesting/LogicTests/WeightLiftingLogicTest.java
+++ tests/src/de/tomgrill/gdxtesting/LogicTests/WeightLiftingLogicTest.java
@@ -19,8 +19,8 @@ public class WeightLiftingLogicTest {
     public void testCalcAcceleration() {
         WeightLiftingLogic Case = new WeightLiftingLogic();
 
-        Case.score = 9;
-        Case.accInput = 3;
+        Case.setScore(9);
+        Case.setAccInput(3);
 
 
         double exp = 3.6;
@@ -28,8 +28,8 @@ public class WeightLiftingLogicTest {
 
         assertEquals(act, exp, 0.001);
 
-        Case.score = 10;
-        Case.accInput = 4;
+        Case.setScore(10);
+        Case.setAccInput(4);
 
         exp = 5.244;
         act = Case.calcAcceleration();
@@ -44,21 +44,21 @@ public class WeightLiftingLogicTest {
         WeightLiftingLogic Case = new WeightLiftingLogic();
         float delta = Gdx.graphics.getDeltaTime();
 
-        Case.trace_x = 1;
-        Case.score = 5;
-        Case.statusBarMinX = 100;
-        Case.statusBarMaxX = 200;
-        Case.liftTimer = 150;
+        Case.setTrace_x(1);
+        Case.setScore(5);
+        Case.setStatusBarMinX(100);
+        Case.setStatusBarMaxX(200);
+        Case.setLiftTimer(150);
 
         assertTrue(Case.wait(delta));
 
-        Case.liftTimer = 0;
+        Case.setLiftTimer(0);
 
         assertFalse(Case.wait(delta));
 
-        assertEquals(Case.liftTimer, 130);
+        assertEquals(Case.getLiftTimer(), 130);
 
-        assertTrue((Case.trace_x) >= 100 && (Case.trace_x) <= 200);
+        assertTrue((Case.getTrace_x()) >= 100 && (Case.getTrace_x()) <= 200);
 
     }
 
@@ -68,35 +68,35 @@ public class WeightLiftingLogicTest {
         float delta = Gdx.graphics.getDeltaTime();
 
 
-        Case.trace_x = 1;
-        Case.score = 5;
-        Case.statusBarMinX = 100;
-        Case.statusBarMaxX = 200;
+        Case.setTrace_x(1);
+        Case.setScore(5);
+        Case.setStatusBarMaxX(200);
+        Case.setLiftTimer(150);
 
-        Case.traceSpeed = (float)0.7;
+        Case.setTraceSpeed((float)0.7);
         Case.lift(delta);
 
-        double act = (double)Case.traceSpeed;
+        double act = (double)Case.getTraceSpeed();
 
         int counter1 = 0, counter2 = 0, counter3 = 0, counter4 = 0, counter5 = 0;
 
         for(int i = 0; i < 160000; ++i)
         {
-            Case.traceSpeed = (float)0.7;
-            Case.liftTimer = 150;
-            Case.trace_x = 101;
+            Case.setTraceSpeed((float)0.7);
+            Case.setLiftTimer(150);
+            Case.setTrace_x(101);
             Case.lift(delta);
 
 
-            if(Math.abs(Case.traceSpeed-0.2) <= 0.001)
+            if(Math.abs(Case.getTraceSpeed()-0.2) <= 0.001)
                 counter1++;
-            else if(Math.abs(Case.traceSpeed-0.4) <= 0.001)
+            else if(Math.abs(Case.getTraceSpeed()-0.4) <= 0.001)
                 counter2++;
-            else if(Math.abs(Case.traceSpeed-0.6) <= 0.001)
+            else if(Math.abs(Case.getTraceSpeed()-0.6) <= 0.001)
                 counter3++;
-            else if(Math.abs(Case.traceSpeed) <= 0.001)
+            else if(Math.abs(Case.getTraceSpeed()) <= 0.001)
                 counter4++;
-            else if(Math.abs(Case.traceSpeed+0.2) <= 0.001)
+            else if(Math.abs(Case.getTraceSpeed()+0.2) <= 0.001)
                 counter5++;
 
         }
@@ -107,37 +107,37 @@ public class WeightLiftingLogicTest {
         assertTrue((counter4 > 14000 && counter2 < 26000));
         assertTrue((counter5 > 14000 && counter2 < 26000));
 
-        Case.traceSpeed = (float)0.7;
-        Case.liftTimer = 150;
-        Case.trace_x = 1;
+        Case.setTraceSpeed((float)0.7);
+        Case.setLiftTimer(150);
+        Case.setTrace_x(1);
         Case.lift(delta);
 
         double exp = -1;
-        assertEquals((double)Case.traceSpeed, exp, 0.001);
+        assertEquals((double)Case.getTraceSpeed(), exp, 0.001);
 
-        Case.traceSpeed = (float)0.7;
-        Case.liftTimer = 150;
-        Case.trace_x = 201;
+        Case.setTraceSpeed((float)0.7);
+        Case.setLiftTimer(150);
+        Case.setTrace_x(201);
         Case.lift(delta);
 
         exp = 1;
-        assertEquals((double)Case.traceSpeed, exp, 0.001);
+        assertEquals((double)Case.getTraceSpeed(), exp, 0.001);
 
-        Case.liftTimer = 0;
-        Case.score = 4;
-        Case.trace_x = 155;
-        Case.statusGreenBarMaxX = 170;
-        Case.statusGreenBarMinX = 130;
+        Case.setLiftTimer(0);
+        Case.setScore(4);
+        Case.setTrace_x(155);
+        Case.setStatusGreenBarMaxX(170);
+        Case.setStatusGreenBarMinX(130);
         boolean ret = Case.lift(delta);
 
         exp = 5;
 
-        assertEquals(exp, Case.score, 0.001);
+        assertEquals(exp, Case.getScore(), 0.001);
         assertFalse(ret);
 
-        Case.liftTimer = 0;
-        Case.score = 4;
-        Case.trace_x = 190;
+        Case.setLiftTimer(0);
+        Case.setScore(4);
+        Case.setTrace_x(190);
 
         ret = Case.lift(delta);
 
